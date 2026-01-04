0311c4b2a0098ac09ca72a90ed92f1ee0131a01c
honoredev <honoretuyizere8@gmail.com>
2026-01-04 14:30:52 +0200

    always updated

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -208,17 +208,6 @@ const HomePage = () => {
     return () => clearInterval(interval);
   }, [news]);
 
-  if (loading) {
-    return (
-      <div className="min-h-screen bg-white flex items-center justify-center">
-        <div className="text-center">
-          <div className="animate-spin h-12 w-12 border-b-2 border-green-600 mx-auto mb-4"></div>
-          <p className="text-gray-600">Loading news...</p>
-        </div>
-      </div>
-    );
-  }
-
   return (
     <div className="min-h-screen bg-white">
       
